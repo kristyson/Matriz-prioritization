86b8fa1ea160e8eb3dc6cecc3b2fb67f0b0b105f
kristyson <131497756+kristyson@users.noreply.github.com>
2024-03-23 15:04:47 -0300

    Perguntas completas
    Foi adicionado todas as perguntas e alternativas na tela Forms

--- Matriz-Priori/app/src/main/java/com/example/matrz_priori/forms.kt
+++ Matriz-Priori/app/src/main/java/com/example/matrz_priori/forms.kt
@@ -1,11 +1,21 @@
 package com.example.matrz_priori
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class forms : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forms)
+
+        val btnProximo: Button = findViewById(R.id.btnProximo)
+
+        btnProximo.setOnClickListener{
+
+
+
+        }
     }
 }
